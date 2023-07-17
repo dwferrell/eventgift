5f9805056cdfd9f530e73cc7961d2dfaf194fde1
dwferrell <2379966+dwferrell@users.noreply.github.com>
2023-07-16 18:57:46 -0700

    aider: Add scan_qr_code function to handle scanning a QR code and updating the user's balance.
    # Aider chat conversation:
    
    USER: Great. Please create or update files.
    
    ASSISTANT: Great, let's proceed to the next step. We need to create the scan_qr_code function. Here's the proposed change:
    
    ```python
    localight_egift/users/views.py
    <<<<<<< ORIGINAL
    from django.shortcuts import render, redirect
    from django.contrib.auth.decorators import login_required
    from .models import User
    import stripe
    
    stripe.api_key = "sk_test_51NUfRnER8Gw2gWMvtgdUZVFTucNKne9Me7OdoCEpR7Ef92r0Q3M6rco9bTl8TGie7vZxBQOuGD6dQ0b4fZV54EbD00mqbXDrkS"
    
    @login_required
    def profile(request):
        user = User.objects.get(pk=request.user.pk)
        return render(request, 'profile.html', {'user': user})
    
    @login_required
    def load_money(request):
        if request.method == 'POST':
            user = User.objects.get(pk=request.user.pk)
            amount = int(request.POST.get('amount'))
            if amount < 10:
                return render(request, 'load_money.html', {'error': 'Minimum load amount is $10.'})
            stripe.Charge.create(
                amount=amount * 100,  # Stripe expects the amount in cents
                currency='usd',
                description=f'Load money for {user.username}',
                source=request.POST.get('stripeToken')
            )
            user.balance += amount
            user.save()
            return redirect('users:profile')
        else:
            return render(request, 'load_money.html')
    =======
    from django.shortcuts import render, redirect
    from django.contrib.auth.decorators import login_required
    from .models import User
    from localight_egift.vendors.models import Vendor
    import stripe
    
    stripe.api_key = "sk_test_51NUfRnER8Gw2gWMvtgdUZVFTucNKne9Me7OdoCEpR7Ef92r0Q3M6rco9bTl8TGie7vZxBQOuGD6dQ0b4fZV54EbD00mqbXDrkS"
    
    @login_required
    def profile(request):
        user = User.objects.get(pk=request.user.pk)
        return render(request, 'profile.html', {'user': user})
    
    @login_required
    def load_money(request):
        if request.method == 'POST':
            user = User.objects.get(pk=request.user.pk)
            amount = int(request.POST.get('amount'))
            if amount < 10:
                return render(request, 'load_money.html', {'error': 'Minimum load amount is $10.'})
            stripe.Charge.create(
                amount=amount * 100,  # Stripe expects the amount in cents
                currency='usd',
                description=f'Load money for {user.username}',
                source=request.POST.get('stripeToken')
            )
            user.balance += amount
            user.save()
            return redirect('users:profile')
        else:
            return render(request, 'load_money.html')
    
    @login_required
    def scan_qr_code(request):
        if request.method == 'POST':
            user = User.objects.get(pk=request.user.pk)
            qr_code = request.POST.get('qr_code')
            vendor = Vendor.objects.get(qr_code=qr_code)
            amount = vendor.qr_code.split('=')[-1]
            user.balance += int(amount)
            user.save()
            return redirect('users:profile')
        else:
            return render(request, 'scan_qr_code.html')
    >>>>>>> UPDATED
    ```
    
    This adds the scan_qr_code function, which handles scanning a QR code and updating the user's balance. It gets the vendor using the QR code from the POST data, and then adds the amount embedded in the QR code to the user's balance.
    
    Please confirm this change so we can proceed to the next step.

--- localight_egift/users/views.py
+++ localight_egift/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import User
+from localight_egift.vendors.models import Vendor
 import stripe
 
 stripe.api_key = "sk_test_51NUfRnER8Gw2gWMvtgdUZVFTucNKne9Me7OdoCEpR7Ef92r0Q3M6rco9bTl8TGie7vZxBQOuGD6dQ0b4fZV54EbD00mqbXDrkS"
@@ -28,3 +29,16 @@ def load_money(request):
         return redirect('users:profile')
     else:
         return render(request, 'load_money.html')
+
+@login_required
+def scan_qr_code(request):
+    if request.method == 'POST':
+        user = User.objects.get(pk=request.user.pk)
+        qr_code = request.POST.get('qr_code')
+        vendor = Vendor.objects.get(qr_code=qr_code)
+        amount = vendor.qr_code.split('=')[-1]
+        user.balance += int(amount)
+        user.save()
+        return redirect('users:profile')
+    else:
+        return render(request, 'scan_qr_code.html')
