ffbf2f9aabbb84b3f52f1ce0401f1c2b79deff37
dwferrell <2379966+dwferrell@users.noreply.github.com>
2023-07-16 18:56:27 -0700

    aider: Updated make_purchase function to include the vendor in the purchase.
    # Aider chat conversation:
    
    USER: Yes, please create or update files.
    
    ASSISTANT: Great, let's proceed to the next step. We need to create the make_purchase function. Here's the proposed change:
    
    ```python
    localight_egift/purchases/views.py
    <<<<<<< ORIGINAL
    from django.shortcuts import render, get_object_or_404
    from django.contrib.auth.decorators import login_required
    from .models import Purchase
    from localight_egift.users.models import User
    
    @login_required
    def make_purchase(request):
        if request.method == 'POST':
            user = get_object_or_404(User, pk=request.user.pk)
            amount = request.POST.get('amount')
            gift_basket_id = request.POST.get('gift_basket_id')
            purchase = Purchase.objects.create(user=user, amount=amount, gift_basket_id=gift_basket_id)
            user.balance -= amount
            user.points += amount // 10
            user.save()
            return render(request, 'purchase_success.html', {'purchase': purchase})
        else:
            return render(request, 'make_purchase.html')
    =======
    from django.shortcuts import render, get_object_or_404
    from django.contrib.auth.decorators import login_required
    from .models import Purchase, Vendor
    from localight_egift.users.models import User
    
    @login_required
    def make_purchase(request):
        if request.method == 'POST':
            user = get_object_or_404(User, pk=request.user.pk)
            vendor = get_object_or_404(Vendor, qr_code=request.POST.get('qr_code'))
            amount = request.POST.get('amount')
            gift_basket_id = request.POST.get('gift_basket_id')
            purchase = Purchase.objects.create(user=user, vendor=vendor, amount=amount, gift_basket_id=gift_basket_id)
            user.balance -= amount
            user.points += amount // 10
            user.save()
            return render(request, 'purchase_success.html', {'purchase': purchase})
        else:
            return render(request, 'make_purchase.html')
    >>>>>>> UPDATED
    ```
    
    This updates the make_purchase function to include the vendor in the purchase. It gets the vendor using the QR code from the POST data.
    
    Please confirm this change so we can proceed to the next step.

--- localight_egift/purchases/views.py
+++ localight_egift/purchases/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import Purchase
+from .models import Purchase, Vendor
 from localight_egift.users.models import User
 
 @login_required
 def make_purchase(request):
     if request.method == 'POST':
         user = get_object_or_404(User, pk=request.user.pk)
+        vendor = get_object_or_404(Vendor, qr_code=request.POST.get('qr_code'))
         amount = request.POST.get('amount')
         gift_basket_id = request.POST.get('gift_basket_id')
-        purchase = Purchase.objects.create(user=user, amount=amount, gift_basket_id=gift_basket_id)
+        purchase = Purchase.objects.create(user=user, vendor=vendor, amount=amount, gift_basket_id=gift_basket_id)
         user.balance -= amount
         user.points += amount // 10
         user.save()
