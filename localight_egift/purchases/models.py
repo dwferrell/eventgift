d0441d0a9f8fc718ed9310c2edbb6437ff630155
dwferrell <2379966+dwferrell@users.noreply.github.com>
2023-07-16 18:54:27 -0700

    aider: Added foreign key to Vendor model in Purchase model.
    # Aider chat conversation:
    
    USER: proceed
    
    ASSISTANT: Great, let's proceed to the next step. We need to update the Purchase model to include a foreign key to the Vendor model. Here's the proposed change:
    
    ```python
    localight_egift/purchases/models.py
    <<<<<<< ORIGINAL
    from django.db import models
    from localight_egift.users.models import User
    
    class Purchase(models.Model):
        user = models.ForeignKey(User, on_delete=models.CASCADE)
        amount = models.DecimalField(max_digits=10, decimal_places=2)
        gift_basket_id = models.IntegerField()
        created_at = models.DateTimeField(auto_now_add=True)
    
        def __str__(self):
            return f'Purchase {self.id} by {self.user.username} for {self.amount}'
    =======
    from django.db import models
    from localight_egift.users.models import User
    from localight_egift.vendors.models import Vendor
    
    class Purchase(models.Model):
        user = models.ForeignKey(User, on_delete=models.CASCADE)
        vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE, null=True, blank=True)
        amount = models.DecimalField(max_digits=10, decimal_places=2)
        gift_basket_id = models.IntegerField()
        created_at = models.DateTimeField(auto_now_add=True)
    
        def __str__(self):
            return f'Purchase {self.id} by {self.user.username} for {self.amount}'
    >>>>>>> UPDATED
    ```
    
    This adds a foreign key to the Vendor model in the Purchase model. The `null=True, blank=True` options are included because the gift basket purchase is not associated with a vendor.
    
    Please confirm this change so we can proceed to the next step.

--- localight_egift/purchases/models.py
+++ localight_egift/purchases/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from localight_egift.users.models import User
+from localight_egift.vendors.models import Vendor
 
 class Purchase(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE, null=True, blank=True)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     gift_basket_id = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
