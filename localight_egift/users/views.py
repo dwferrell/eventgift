357774637e5d578d6b7894841572a42cebfb523c
dwferrell <2379966+dwferrell@users.noreply.github.com>
2023-07-16 18:55:38 -0700

    aider: Added load_money function to handle loading money into a user's account using the Stripe API.
    # Aider chat conversation:
    
    USER: Proceed. Also, here is a Stripe sandbox key that you can put in a new settings file. Publishable key: "pk_test_51NUfRnER8Gw2gWMvxCE8SJaeN1pMb8smHeFCz7745ZU6M2zHVRYdCRXMcdmb8Aqoof06Z0ohwiCRfva9wpwWsxqu00ghHGbcZM" Secret key: "sk_test_51NUfRnER8Gw2gWMvtgdUZVFTucNKne9Me7OdoCEpR7Ef92r0Q3M6rco9bTl8TGie7vZxBQOuGD6dQ0b4fZV54EbD00mqbXDrkS"
    
    ASSISTANT: Great, let's proceed to the next step. We need to create the load_money, make_purchase, scan_qr_code, and purchase_gift_basket functions.
    
    Let's start with the load_money function. Here's the proposed change:
    
    ```python
    localight_egift/users/views.py
    <<<<<<< ORIGINAL
    from django.shortcuts import render
    from django.contrib.auth.decorators import login_required
    from .models import User
    
    @login_required
    def profile(request):
        user = User.objects.get(pk=request.user.pk)
        return render(request, 'profile.html', {'user': user})
    =======
    from django.shortcuts import render, redirect
    from django.contrib.auth.decorators import login_required
    from .models import User
    import stripe
    
    stripe.api_key = "sk_test_51NUfRnER8Gw2gWMvtgdUZVFTucNKne9Me7OdoCEpR7Ef92r0Q3M6rco9bTl8TGie7vZxBQOuGD6dQ0b4fZV54EbD00mqbXDrkS"
    
    @login_required
    def profile(request):
        user = User.objects.get(pk=request.user.pk)
        return render(request, 'profile.html', {'user': user})
    
    @login_required
    def load_money(request):
        if request.method == 'POST':
            user = User.objects.get(pk=request.user.pk)
            amount = int(request.POST.get('amount'))
            if amount < 10:
                return render(request, 'load_money.html', {'error': 'Minimum load amount is $10.'})
            stripe.Charge.create(
                amount=amount * 100,  # Stripe expects the amount in cents
                currency='usd',
                description=f'Load money for {user.username}',
                source=request.POST.get('stripeToken')
            )
            user.balance += amount
            user.save()
            return redirect('users:profile')
        else:
            return render(request, 'load_money.html')
    >>>>>>> UPDATED
    ```
    
    This adds the load_money function, which handles loading money into a user's account. It uses the Stripe API to create a charge, and then updates the user's balance. The function is decorated with the @login_required decorator to ensure that only logged-in users can load money.
    
    Please confirm this change so we can proceed to the next step.

--- localight_egift/users/views.py
+++ localight_egift/users/views.py
@@ -1,8 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import User
+import stripe
+
+stripe.api_key = "sk_test_51NUfRnER8Gw2gWMvtgdUZVFTucNKne9Me7OdoCEpR7Ef92r0Q3M6rco9bTl8TGie7vZxBQOuGD6dQ0b4fZV54EbD00mqbXDrkS"
 
 @login_required
 def profile(request):
     user = User.objects.get(pk=request.user.pk)
     return render(request, 'profile.html', {'user': user})
+
+@login_required
+def load_money(request):
+    if request.method == 'POST':
+        user = User.objects.get(pk=request.user.pk)
+        amount = int(request.POST.get('amount'))
+        if amount < 10:
+            return render(request, 'load_money.html', {'error': 'Minimum load amount is $10.'})
+        stripe.Charge.create(
+            amount=amount * 100,  # Stripe expects the amount in cents
+            currency='usd',
+            description=f'Load money for {user.username}',
+            source=request.POST.get('stripeToken')
+        )
+        user.balance += amount
+        user.save()
+        return redirect('users:profile')
+    else:
+        return render(request, 'load_money.html')
